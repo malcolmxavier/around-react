e446d1077d8978bc560d641090dfa378bb5878c4
Malcolm Xavier <malcolm.x.evans@gmail.com>
2020-09-06 11:04:30 -0400

    fix app.js

--- src/components/App.js
+++ src/components/App.js
@@ -43,37 +43,35 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <body className='page'>
-        <Header />
-        <Main 
-          onEditAvatar={handleEditAvatarClick}
-          onEditProfile={handleEditProfileClick}
-          onAddCard={handleAddCardClick}
-          onCard={handleCardClick}
-          onDeleteCard={handleDeleteCardClick}
-          selectedCard={selectedCard}
-        ></Main>
-        <PopupWithForm name='edit-avatar' action='Edit Avatar' title='Change profile picture' button='save' onClose={closeAllPopups} isOpen={isEditAvatarPopupOpen}>
-                <input name='avatar' id='avatar-url' className='popup__input popup__input_type_url' type='url' placeholder='Image URL' required />
-                <span id='avatar-url-error' className='popup__error'></span>
-        </PopupWithForm>
-        <PopupWithForm name='edit-profile' action='Edit Profile' title='Edit profile' button='save' onClose={closeAllPopups} isOpen={isEditProfilePopupOpen}>
-                <input name='userName' id='profile-name' className='popup__input popup__input_type_name' type='text' placeholder='Name' required minLength="2" maxLength="40" />
-                <span id='profile-name-error' className='popup__error'></span>
-                <input name='userOccupation' id='profile-occupation' className='popup__input popup__input_type_occupation' type='text' placeholder='About Me' required minLength="2" maxLength="200" />
-                <span id='profile-occupation-error' className='popup__error'></span>
-        </PopupWithForm>
-        <PopupWithForm name='add-card' action='Add Card' title='New Place' button='create' onClose={closeAllPopups} isOpen={isAddCardPopupOpen}>
+    <div className="page">
+      <Header />
+      <Main 
+        onEditAvatar={handleEditAvatarClick}
+        onEditProfile={handleEditProfileClick}
+        onAddCard={handleAddCardClick}
+        onCard={handleCardClick}
+        onDeleteCard={handleDeleteCardClick}
+        selectedCard={selectedCard}
+      ></Main>
+      <PopupWithForm name='edit-avatar' action='Edit Avatar' title='Change profile picture' button='save' onClose={closeAllPopups} isOpen={isEditAvatarPopupOpen}>
+        <input name='avatar' id='avatar-url' className='popup__input popup__input_type_url' type='url' placeholder='Image URL' required />
+        <span id='avatar-url-error' className='popup__error'></span>
+      </PopupWithForm>
+      <PopupWithForm name='edit-profile' action='Edit Profile' title='Edit profile' button='save' onClose={closeAllPopups} isOpen={isEditProfilePopupOpen}>
+        <input name='userName' id='profile-name' className='popup__input popup__input_type_name' type='text' placeholder='Name' required minLength="2" maxLength="40" />
+        <span id='profile-name-error' className='popup__error'></span>
+        <input name='userOccupation' id='profile-occupation' className='popup__input popup__input_type_occupation' type='text' placeholder='About Me' required minLength="2" maxLength="200" />
+        <span id='profile-occupation-error' className='popup__error'></span>
+      </PopupWithForm>
+      <PopupWithForm name='add-card' action='Add Card' title='New Place' button='create' onClose={closeAllPopups} isOpen={isAddCardPopupOpen}>
         <input name='name' id='card-label' className='popup__input popup__input_type_label' type='text' placeholder='Label' required minLength="1" maxLength="30" />
         <span id='card-label-error' className='popup__error'></span>
         <input name='link' id='card-url' className='popup__input popup__input_type_url' type='url' placeholder='Image URL' required />
         <span id='card-url-error' className='popup__error'></span>
-        </PopupWithForm>
-        <PopupWithImage image='' title='Image Caption' card={selectedCard} onClose={closeAllPopups} isOpen={isImagePopupOpen}/>
-        <PopupWithForm name="delete-card" action='Delete Card' title='Are you sure?' button='yes' onClose={closeAllPopups} isOpen={isDeleteCardPopupOpen}/>
-        <Footer />
-      </body>
+      </PopupWithForm>
+      <PopupWithImage image='' title='Image Caption' card={selectedCard} onClose={closeAllPopups} isOpen={isImagePopupOpen}/>
+      <PopupWithForm name="delete-card" action='Delete Card' title='Are you sure?' button='yes' onClose={closeAllPopups} isOpen={isDeleteCardPopupOpen}/>
+      <Footer />
     </div>
   );
 }
